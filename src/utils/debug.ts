c2975a7806ec53b7107070c15cedc295329122c9
blitz-1306 <hyperx27@gmail.com>
2023-10-13 18:38:11 +0500

    Add doc string to debug functions

--- src/utils/debug.ts
+++ src/utils/debug.ts
@@ -2,6 +2,12 @@ import { walk } from ".";
 import { Program } from "../interp";
 import { Node } from "../ir";
 
+/**
+ * Walk thourgh an each node of passed `program` and compute its
+ * parents (nodes that are referring to the checked one it their subtree).
+ *
+ * Returns map from each node to an array of referring nodes.
+ */
 export function computeNodeParents(program: Program): Map<Node, Node[]> {
     const res = new Map<Node, Node[]>();
 
@@ -24,6 +30,17 @@ export function computeNodeParents(program: Program): Map<Node, Node[]> {
     return res;
 }
 
+/**
+ * Apply `computeNodeParents()` to a program and preserve only entries,
+ * where one node haves mupltiple parents (is referred by several other node subtrees).
+ *
+ * It may be considered abnormal if same node is referred twice in same program.
+ * Such case may indicate an issue in tree building logic,
+ * as changing one node may have cascade effect in unpredictable places.
+ *
+ * Only exception from this rule if node is reused intentionally
+ * (like predefined reserved type reference).
+ */
 export function findMultiParentNodes(program: Program): Map<Node, Node[]> {
     const res = computeNodeParents(program);
 
