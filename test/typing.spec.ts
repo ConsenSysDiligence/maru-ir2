4fb86adf7a1b1ad3c09107a7980b6ab0dca12060
Dimitar Bounov <dimitar.bounov@consensys.net>
2023-10-22 15:49:15 -1000

    Add monomorphizing test

--- test/typing.spec.ts
+++ test/typing.spec.ts
@@ -6,66 +6,110 @@ import {
     Identifier,
     MemVariableDeclaration,
     MIRTypeError,
+    Monomorphize,
     parseProgram,
+    pp,
+    Program,
     Resolving,
     Typing,
     walk
 } from "../src";
 import { searchRecursive } from "./utils";
 
+function skipIdentifiers(program: Program): Set<string> {
+    const res = new Set<string>(
+        program
+            .filter((def): def is FunctionDefinition => def instanceof FunctionDefinition)
+            .map((def) => def.name)
+    );
+
+    for (const def of program) {
+        walk(def, (n) => {
+            if (n instanceof MemVariableDeclaration) {
+                res.add(n.name);
+            }
+        });
+    }
+
+    return res;
+}
+
 describe("Typing positive tests", () => {
     const files = searchRecursive("test/samples/valid", (name) => name.endsWith(".maruir"));
 
     for (const file of files) {
-        it(file, () => {
-            const contents = fse.readFileSync(file, { encoding: "utf-8" });
-            const program = parseProgram(contents);
-            const resolving = new Resolving(program);
-            const typing = new Typing(program, resolving);
+        describe(`${file}`, () => {
+            let program: Program;
+            let resolving: Resolving;
+            let typing: Typing;
 
-            const funNames = new Set<string>(
-                program
-                    .filter((def): def is FunctionDefinition => def instanceof FunctionDefinition)
-                    .map((def) => def.name)
-            );
+            before(() => {
+                const contents = fse.readFileSync(file, { encoding: "utf-8" });
+                program = parseProgram(contents);
+                resolving = new Resolving(program);
+            });
 
-            const memVarNames = new Set<string>();
+            it(`Typing succeeds and covers all expressions`, () => {
+                typing = new Typing(program, resolving);
 
-            for (const def of program) {
-                walk(def, (n) => {
-                    if (n instanceof MemVariableDeclaration) {
-                        memVarNames.add(n.name);
+                const skip = skipIdentifiers(program);
+
+                for (const def of program) {
+                    if (!(def instanceof FunctionDefinition && def.body)) {
+                        continue;
                     }
-                });
-            }
 
-            for (const def of program) {
-                if (!(def instanceof FunctionDefinition && def.body)) {
-                    continue;
-                }
+                    for (const bb of def.body.nodes.values()) {
+                        for (const stmt of bb.statements) {
+                            walk(stmt, (nd) => {
+                                // Skip function names in function calls and mem vars
+                                if (nd instanceof Identifier && skip.has(nd.name)) {
+                                    return;
+                                }
 
-                for (const bb of def.body.nodes.values()) {
-                    for (const stmt of bb.statements) {
-                        walk(stmt, (nd) => {
-                            // Skip function names in function calls
-                            if (nd instanceof Identifier && funNames.has(nd.name)) {
-                                return;
-                            }
-
-                            // Skip mem vars uses in alloc
-                            if (nd instanceof Identifier && memVarNames.has(nd.name)) {
-                                return;
-                            }
-
-                            if (nd instanceof Expression) {
-                                const type = typing.typeOf(nd);
-                                // console.error(`Type of ${nd.pp()} is ${pp(type)}`);
-                                expect(type).toBeDefined();
-                            }
-                        });
+                                if (nd instanceof Expression) {
+                                    const type = typing.typeOf(nd);
+                                    // console.error(`Type of ${nd.pp()} is ${pp(type)}`);
+                                    expect(type).toBeDefined();
+                                }
+                            });
+                        }
                     }
                 }
-            }
+            });
+
+            it(`Typing succeeds and covers all expressions after monomorphizing`, () => {
+                const mono = new Monomorphize(program, resolving);
+                const newProgram = mono.run();
+                console.error(`Mono program: `, pp(newProgram));
+                const newResolving = new Resolving(newProgram);
+                const newTyping = new Typing(newProgram, newResolving);
+
+                const skip = skipIdentifiers(newProgram);
+
+                for (const def of newProgram) {
+                    if (!(def instanceof FunctionDefinition && def.body)) {
+                        continue;
+                    }
+
+                    for (const bb of def.body.nodes.values()) {
+                        for (const stmt of bb.statements) {
+                            walk(stmt, (nd) => {
+                                // Skip function names in function calls and mem vars
+                                if (nd instanceof Identifier && skip.has(nd.name)) {
+                                    return;
+                                }
+
+                                if (nd instanceof Expression) {
+                                    const type = newTyping.typeOf(nd);
+                                    console.error(`Type of ${nd.pp()} is ${pp(type)}`);
+                                    expect(type).toBeDefined();
+                                }
+                            });
+                        }
+                    }
+                }
+            });
         });
     }
 });
